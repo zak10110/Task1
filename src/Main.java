afb59905c24de0fc0a0aba1953ca37e38af87b61
zak10110 <zr380678644997@gmail.com>
2025-01-31 19:46:04 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,28 @@
-
+import java.util.Scanner;
 public class Main {
         public static void main(String[] args) {
+                Scanner scanner=new Scanner(System.in);
+                System.out.println("Введите пятизначное число:");
+                int[] arr=task1(scanner.nextInt());
+                scanner.close();
+                for(int num:arr){
+                        System.out.println(num+"\n");
+                }
+                System.out.println(task2(5,10,10));
+        }
 
-
+        public static int[] task1(int num){
+                int splitFactor=10000;
+                int[] arr=new int[5];
+                for (int i=0;i<5;i++){
+                        arr[i]=(num/splitFactor)%10;
+                        splitFactor/=10;
+                }
+                return arr;
+        }
+        public static double task2(double a,double b,double c){
+                double p=(a + b + c) /2;
+                double s=Math.sqrt(p*(p-a)*(p-b)*(p-c));
+                return s;
         }
 }
